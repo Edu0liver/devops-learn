e3e64826e36406792b466895eb6707330db81145
Eduardo Oliveira Alves <eduardo.alves@icc.org.br>
2024-01-10 17:25:50 -0300

    vpc created

--- terraform-own/modules/vpc/main.tf
+++ terraform-own/modules/vpc/main.tf
@@ -1,17 +1,47 @@
-resource "aws_vpc" "my-vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name = "${var.prefix}-vpc}"
+    Name = "${var.prefix}-vpc"
   }
 }
 
 data "aws_availability_zones" "available" {
-    state = "available"
+  state = "available"
 }
 
 resource "aws_subnet" "my-subnet" {
-    count = 2
-    vpc_id            = aws_vpc.my-vpc.id
-    cidr_block        = var.vpc_cidr_block
-    availability_zone = data.aws_availability_zones.available.names[count.index]
+  count = 3
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "${var.prefix}-subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "my-rtb" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "my-rtb-association-subnets" {
+  count = 3
+  subnet_id = aws_subnet.my-subnet.*.id[count.index]
+  route_table_id = aws_route_table.my-rtb.id
 }
