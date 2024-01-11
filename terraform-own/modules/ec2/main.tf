f9887a80de838921334cfe753c5f240a22682c45
Eduardo Oliveira Alves <eduardo.alves@icc.org.br>
2024-01-11 17:24:34 -0300

    http to ec2

--- terraform-own/modules/ec2/main.tf
+++ terraform-own/modules/ec2/main.tf
@@ -21,13 +21,13 @@ resource "aws_security_group" "allow_ssh_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   description = "HTTP to EC2"
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+    description = "HTTP to EC2"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
